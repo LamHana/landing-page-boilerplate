6027b4a26a2b303cb06023672cae70d04a4c5f11
LamHana <lamtngochan@gmail.com>
2026-04-09 16:36:44 +0700

    feat(template): add dark mode optional feature module
    - Add theme toggle component for light/dark switching
    - Add layout injection for app integration
    - Include package metadata

---
+++ templates/nextjs/optional/dark-mode/inject/app__layout.tsx
@@ -0,0 +1,8 @@
+MARKER:__PROVIDERS_IMPORT__
+import { ThemeProvider } from "next-themes";
+---
+MARKER:__PROVIDERS_WRAP_START__
+<ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+---
+MARKER:__PROVIDERS_WRAP_END__
+</ThemeProvider>
